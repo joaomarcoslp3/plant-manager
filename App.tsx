c141592688898a32db42601897906a5f0034f9b5
João Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2021-04-20 20:34:07 -0300

    created initial application flow

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AppLoading from 'expo-app-loading'
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
 
-import { Welcome } from './src/pages/'
+import Routes from './src/routes/'
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -13,6 +13,6 @@ export default function App() {
   if (!fontsLoaded) return <AppLoading />
 
   return (
-    <Welcome />
+    <Routes />
   )
 }
---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, SafeAreaView, Text, TextInput, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { useNavigation } from '@react-navigation/core'
+import { Button } from '../components'
+import { colors, fonts } from '../styles'
+
+export function UserIdentification () {
+  const [name, setName] = useState<string>('')
+  const [isFilled, setIsFilled] = useState(false)
+  const [error, setError] = useState(false)
+  const [isFocused, setIsFocused] = useState(false)
+  const { navigate } = useNavigation()
+
+  useEffect(() => {
+    if (error){
+      setError(false)
+    }
+  }, [name])
+  
+  function handleInputBlur () {
+    setIsFocused(false)
+    setIsFilled(!!name)
+  }
+
+  function handleInputFocus () {
+    setIsFocused(true)
+  }
+
+  function handleInputChange (value: string) {
+    setIsFilled(!!value)
+    setName(value)
+  }
+
+  function handleSubmit () {
+    if (!name) {
+      setError(true) 
+      return
+    }
+    navigate('Confirmation')
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <View style={styles.header}>
+                <Text style={styles.emoji}>
+                  {error ? '😔' : isFilled ? '😄' : '😀'}
+                </Text>
+                
+                <Text style={styles.title}>Como podemos {'\n'} chamar você?</Text>
+              </View>
+
+              <TextInput 
+                style={[
+                  styles.input,
+                  (isFocused || isFilled) && { borderColor: colors.green },
+                  error && { borderColor: colors.red }
+                ]} 
+                placeholder={error ? 'Não esqueça do nome' : 'Digite um nome'}
+                placeholderTextColor={error ? colors.red : colors.heading}
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
+
+              <View style={styles.footer}>
+                <Button label='Confirmar' onPress={handleSubmit} />
+              </View> 
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  content: {
+    flex: 1,
+    width: '100%'
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 54,
+    alignItems: 'center',
+  },
+  header: {
+    alignItems: 'center'
+  },
+  emoji: {
+    fontSize: 44
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 32,
+    marginTop: 24
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: '100%',
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: 'center'
+  },
+  footer: {
+    width: '100%',
+    marginTop: 40,
+    paddingHorizontal: 20
+  }
+})
